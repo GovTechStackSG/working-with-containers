cb31b4810b25fab87d9d6de587d35b2549ac40c2
Samuel Loh Yin Kang <samuel.loh.yk@gmail.com>
2018-07-24 18:31:20 +0800

    Fixing for running within Openshift

--- frontend/entrypoint.sh
+++ frontend/entrypoint.sh
@@ -1,8 +1,11 @@
-#!/bin/bash
+#!/bin/bash -x
 FRONTEND_PORT=${FRONTEND_PORT:-8000}
 BACKEND_HOSTNAME=${BACKEND_HOSTNAME:-backend}
 BACKEND_PORT=${BACKEND_PORT:-3000}
 
-sed -i "s%FRONTEND_PORT%${FRONTEND_PORT}%;s%BACKEND_HOSTNAME%${BACKEND_HOSTNAME}%;s%BACKEND_PORT%${BACKEND_PORT}%" /etc/nginx/conf.d/default.conf
-nginx -g "daemon off;"
+sed -i -e "s/FRONTEND_PORT/${FRONTEND_PORT}/" \
+-e "s/BACKEND_HOSTNAME/${BACKEND_HOSTNAME}/" \
+-e "s/BACKEND_PORT/${BACKEND_PORT}/" \
+/etc/nginx/conf.d/default.conf
 
+nginx -g "daemon off;"
