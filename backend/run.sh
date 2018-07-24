ba4222e160e4ed19842ec17726a14d03850552f1
Samuel Loh Yin Kang <samuel.loh.yk@gmail.com>
2018-07-24 18:07:35 +0800

    Fixing for running within Openshift

--- backend/run.sh
+++ backend/run.sh
@@ -4,4 +4,5 @@ docker network create demo-net
 
 docker container run --rm -ti --publish 3000:3000 \
 --network demo-net \
+-u 30000 \
 --name backend backend:latest $@
--- frontend/run.sh
+++ frontend/run.sh
@@ -2,4 +2,5 @@
 
 docker container run -ti --rm --publish 8000:8000 \
 --network demo-net \
+-u 30000 \
 --name frontend frontend:latest $@
